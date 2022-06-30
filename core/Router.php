6847099d5edd57437cc22d4e50f7e33358b571ef
MishaR <mesharoman12@gmail.com>
2022-06-30 17:07:32 +0300

    Implemented routing

--- core/Router.php
+++ core/Router.php
@@ -4,5 +4,28 @@ namespace app\core;
 
 class Router
 {
-	
+	private Request $request;
+	private array $routes = [];
+
+	public function __construct(Request $request)
+	{
+		$this->request = $request;
+	}
+
+	public function get($path, $callback)
+	{
+		$this->routes['get'][$path] = $callback;
+	}
+
+	public function resolve()
+	{
+		$path = $this->request->getPath();
+		$method = $this->request->getMethod();
+		$callback = $this->routes[$method][$path] ?? false;
+		
+		if (!$callback) {
+            return 'Not Found';
+        }
+		echo call_user_func($callback);
+	}
 }
