b8b9cba4559f30bac4ab78285bb34879fe4a5da9
MishaR <mesharoman12@gmail.com>
2022-07-04 11:29:09 +0300

    Sanitize  and  data

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Application;
+use app\core\Request;
 
 class SiteController extends Controller
 {
@@ -20,8 +21,9 @@ class SiteController extends Controller
 		return $this->render('contact');
 	}
 
-	public function handleContact()
+	public function handleContact(Request $request)
 	{
-		return 'Handl';
+		$body = $request->getBody();
+		return $body;
 	}
 }
